38731caee6bb6d8292d190b4a46fdc2e03811e12
AlenaNovik <elena.pigaleva@gmail.com>
2021-08-11 14:52:59 +0300

    Readme added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # iTechArt-lab-NodeJS
+
+### Task Exploring how expressJS works, parameters and server error handling
+[Link](https://itechart-lab-nodejs.herokuapp.com/api/greetings?name=Alena)
+
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (request, response) => {    
-      response.send(`<h1>Hello </h1>`);    
-  });
-app.get("/api/greetings", (request, response) => {
-  let userName = request.query.name;
+app.get("/", (request, response) => {
+  response.send(`<h1>Hello </h1>`);
+});
+app.get("/api/greetings", (request: express.Request, response: express.Response) => {
+  const userName = request.query.name;
   if (userName === undefined) {
     response.setHeader("Content-Type", "application/json");
-    response.status(400).send(JSON.stringify({ name: null, message:"The name was not indicated" }, null, 3));
+    response
+      .status(400)
+      .send(
+        JSON.stringify(
+          { name: null, message: "The name was not indicated" },
+          null,
+          3
+        )
+      );
   } else {
     response.send(`<h1>Hello ${userName}</h1>`);
   }
